bfef731c77b68bd4001427c59ca190b85e1f854e
Sabarivel M <sabarimuthu290@gmail.com>
2026-02-28 12:42:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Vertical Tree Traversal/vertical-tree-traversal.java
@@ -0,0 +1,45 @@
+/*
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+class Pair{
+    Node node;
+    int hd;
+    Pair(Node n,int h){
+        node=n;
+        hd=h;
+    }
+}
+
+class Solution {
+    public ArrayList<ArrayList<Integer>> verticalOrder(Node root) {
+        // code here
+        ArrayList<ArrayList<Integer>> ans=new ArrayList<>();
+        if(root==null)return ans;
+        Queue<Pair>q=new LinkedList<>();
+        TreeMap<Integer,ArrayList<Integer>>map=new TreeMap<>();
+        q.offer(new Pair(root,0));
+        while(!q.isEmpty()){
+            Pair p=q.poll();
+            Node node=p.node;
+            int hd=p.hd;
+             map.putIfAbsent(hd, new ArrayList<>());
+            map.get(hd).add(node.data);
+            if(node.left!=null)q.offer(new Pair(node.left,hd-1));
+            if(node.right!=null)q.offer(new Pair(node.right,hd+1));
+        }
+        for(ArrayList<Integer> val:map.values()){
+            ans.add(val);
+        }
+        return ans;
+    }
+}
