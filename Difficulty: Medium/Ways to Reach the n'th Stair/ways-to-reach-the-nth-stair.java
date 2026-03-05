8c515a9e3b14873359ccb20951ad0b285d2dfb7b
Sabarivel M <sabarimuthu290@gmail.com>
2026-03-05 20:33:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int func(int n,int[] dp){
+        if(n<=1)return 1;
+        if(dp[n]!=0)return dp[n];
+        return dp[n]=func(n-1,dp)+func(n-2,dp);
+    }
+    int countWays(int n) {
+        // your code here
+        int[] dp=new int[n+1];
+        return func(n,dp);
+    }
+}
