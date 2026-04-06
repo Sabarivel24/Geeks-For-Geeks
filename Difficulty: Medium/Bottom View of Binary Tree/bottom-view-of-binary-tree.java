8c3d330fd53dc88eda5c5d2935382624881ca76b
Sabarivel M <sabarimuthu290@gmail.com>
2026-04-06 11:47:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.java
+++ Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.java
@@ -12,34 +12,23 @@ class Node {
 }
 */
 
-class Pair{
-    Node node;
-    int hd;
-    Pair(Node n,int h){
-        node=n;
-        hd=h;
-    }
-}
-
 class Solution {
     public ArrayList<Integer> bottomView(Node root) {
-        // code here
-         ArrayList<Integer>ans=new ArrayList<>();
-        if(root==null)return ans;
-        Queue<Pair>q=new LinkedList<>();
-        TreeMap<Integer,Integer>map=new TreeMap<>();
-        q.offer(new Pair(root,0));
-        while(!q.isEmpty()){
-            Pair p=q.poll();
-            Node node=p.node;
-            int hd=p.hd;
-            map.put(hd,node.data);
-            if(node.left!=null)q.offer(new Pair(node.left,hd-1));
-            if(node.right!=null)q.offer(new Pair(node.right,hd+1));
-        }
-        for(int val:map.values()){
-            ans.add(val);
-        }
-        return ans;
+       ArrayList<Integer>ans=new ArrayList<>();
+       Queue<Object[]>q=new LinkedList<>();
+       Map<Integer,Integer>map=new TreeMap<>();
+       q.offer(new Object[]{root,0});
+       while(!q.isEmpty()){
+           Object arr[]=q.poll();
+           Node node=(Node)arr[0];
+           int col=(int)arr[1];
+           map.put(col,node.data);
+           if(node.left!=null) q.offer(new Object[]{node.left,col-1});
+           if(node.right!=null) q.offer(new Object[]{node.right,col+1});
+       }
+       for(int x:map.values()){
+           ans.add(x);
+       }
+       return ans; 
     }
 }
