b0d7b05776bf2f12c0b6fbe0575fc1e5e7f4689f
Sabarivel M <sabarimuthu290@gmail.com>
2026-04-17 13:59:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Sum in a Falling Path/minimum-sum-in-a-falling-path.java
+++ Difficulty: Medium/Minimum Sum in a Falling Path/minimum-sum-in-a-falling-path.java
@@ -1,31 +1,33 @@
 class Solution {
     public int minFallingPathSum(int[][] mat) {
-        // code here
         int n=mat.length;
-        int[][] dp=new int[n+1][n+1];
-        for(int i=0;i<n;i++){
-            dp[0][i]=mat[0][i];
-        }
-        for(int i=1;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(j==0){
-                    dp[i][j]=mat[i][j]+Math.min(dp[i-1][j],dp[i-1][j+1]);;
-                }
-                else if(j==n-1){
-                    dp[i][j]=mat[i][j]+Math.min(dp[i-1][j],dp[i-1][j-1]);
-                }else{
-                    int ld=Math.min(dp[i-1][j],dp[i-1][j-1]);
-                    int rd=Math.min(dp[i-1][j],dp[i-1][j+1]);
-                    int min=Math.min(ld,rd);
-                   dp[i][j]=mat[i][j]+min; 
-                }
-            }
-        }
-        int ans=Integer.MAX_VALUE;
-        for(int i=0;i<n;i++){
-            ans=Math.min(ans,dp[n-1][i]);
-        }
+        int m=mat[0].length;
+       int dp[][]=new int[n][m];
+      
+       for(int i=0;i<m;i++){
+        dp[0][i]=mat[0][i];
+       }
+       for(int i=1;i<n;i++){
+           for(int j=0;j<m;j++){
+              int m1=0;
+              int m2=Integer.MAX_VALUE;
+              int m3=Integer.MAX_VALUE;
+              int min=Integer.MAX_VALUE;
+                 m1=dp[i-1][j]+mat[i][j];
+               if( j+1<m){
+                   m2=dp[i-1][j+1]+mat[i][j];
+               }if(j-1>=0){
+                   m3=dp[i-1][j-1]+mat[i][j];
+               }
+               min=Math.min(m1,m2);
+               min=Math.min(min,m3);
+               dp[i][j]=min;
+           }}
+           int ans=Integer.MAX_VALUE;
+       for(int i=0;i<m;i++){
+           ans=Math.min(ans,dp[n-1][i]);
+       }
+      
         return ans;
-        
     }
 }
